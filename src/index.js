e12427a882c524dd71a5f63431181ee461124bf6
Kalenik Alexander <kalenik.wr@gmail.com>
2018-09-15 12:35:51 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
+class SudokuSolver {
+  constructor(matrix) {
+    this.matrix = matrix;
+  }
+
+  getBox(y, x) {
+    y -= y % 3;
+    x -= x % 3;
+
+    let box = [];
+    for (let i = y; i < y + 3; i++) {
+      box = box.concat(this.matrix[i].slice(x, x + 3));
+    }
+
+    return box;
+  }
+
+  getColumn(x) {
+    let column = Array(this.matrix.length);
+    for (let i = 0; i < this.matrix.length; i++) {
+      column[i] = this.matrix[i][x];
+    }
+    return column;
+  }
+
+  validate(arr) {
+    let s = new Set();
+    return arr.every(i => {
+      if (i === 0 || s.has(i) === false) {
+        s.add(i);
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
+
+  solve() {
+    let y, x;
+    let f = false;
+    for (let i = 0; !f && i < this.matrix.length; i++) {
+      for (let j = 0; !f && j < this.matrix[i].length; j++) {
+        if (this.matrix[i][j] === 0) (y = i), (x = j), (f = true);
+      }
+    }
+
+    if (!f) return true;
+
+    for (let n = 1; n <= 9; n++) {
+      this.matrix[y][x] = n;
+      let row = this.matrix[y];
+      let column = this.getColumn(x);
+      let box = this.getBox(y, x);
+      if (this.validate(row) && this.validate(column) && this.validate(box)) {
+        if (this.solve()) return true;
+      }
+    }
+
+    this.matrix[y][x] = 0;
+  }
+
+  static solve(matrix) {
+    new SudokuSolver(matrix).solve();
+    return matrix;
+  }
 }
+
+module.exports = function solveSudoku(matrix) {
+  return SudokuSolver.solve(matrix);
+};
